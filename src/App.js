7c4ce22fb797c73edc92bf160ba2df4f9679b9f0
Ram <raamkinkar.rout533@gmail.com>
2022-01-29 14:12:02 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Starred from "./pages/Starred";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/starred" element={<Starred />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Starred.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Starred = () => {
+  return <div>I amm Starred Page</div>;
+};
+
+export default Starred;
