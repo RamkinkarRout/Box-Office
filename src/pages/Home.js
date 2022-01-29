771bd5146a275c67f34940154a519ef725a399e9
Ram <raamkinkar.rout533@gmail.com>
2022-01-29 20:31:51 +0530

    Displayed Fetch result and Fetching refractor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,49 @@
 import React, { useState } from "react";
 import MainPageLayout from "../components/MainPageLayout";
+import { apiGet } from "../components/misc/config";
 
 const Home = () => {
   const [input, setInput] = useState("");
-
+  const [results, setresults] = useState(null);
+  // saving input field text to our useState
   const onInputChange = (ev) => {
     setInput(ev.target.value);
   };
 
+  //fetching data
   const onSearch = () => {
     // https://api.tvmaze.com/search/shows?q=girls
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then((r) => r.json())
-      .then((result) => {
-        console.log(result);
-      });
-  };
 
+    apiGet(`/search/shows?q=${input}`).then((result) => {
+      setresults(result);
+      console.log(result);
+    });
+  };
+  //keycode for enter to show results
   const keyDown = (ev) => {
     if (ev.keyCode === 13) {
       onSearch();
     }
   };
+  //conditional rendering according to search text if the results ll be null
+
+  const renderResult = () => {
+    if (results && results.length === 0) {
+      return <div>No Results Found 😓</div>;
+    }
+
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map((item) => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
     <MainPageLayout>
       <input
@@ -33,6 +55,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResult()}
     </MainPageLayout>
   );
 };
