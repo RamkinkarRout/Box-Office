6228ac5bc7a52ea91d3baf354c1a8d904c16faa9
Ram <raamkinkar.rout533@gmail.com>
2022-01-30 21:38:52 +0530

    Added isLoading , error message and isMounted for show

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -6,13 +6,37 @@ const Show = () => {
   const { id } = useParams();
   //   console.log(params);
   const [show, setShow] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then((results) => {
-      setShow(results);
-    });
+    let isMounted = true;
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then((results) => {
+        setTimeout(() => {
+          if (isMounted) {
+            setShow(results);
+            setIsLoading(false);
+          }
+        }, 2000);
+      })
+      .catch((err) => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
   console.log(show);
+  if (isLoading) {
+    return <div>Please wait for a while ⌚</div>;
+  }
+  if (error) {
+    return <div>Error Occured: {error}😖</div>;
+  }
   return <div>showp page</div>;
 };
 
