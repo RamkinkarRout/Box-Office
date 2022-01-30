ed8dea92a4252d538e8039242cb7d9ff8360cae2
Ram <raamkinkar.rout533@gmail.com>
2022-01-30 23:56:39 +0530

    Styled and fetched details of showMainPage

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { apiGet } from "../components/misc/config";
+import Cast from "../components/shows/Cast";
+import Details from "../components/shows/Details";
+import Season from "../components/shows/Season";
+import ShowMainData from "../components/shows/ShowMainData";
+import { InfoBlock, ShowPageWrapper } from "./Show.styled";
 
 const Show = () => {
   const { id } = useParams();
@@ -18,7 +23,7 @@ const Show = () => {
             setShow(results);
             setIsLoading(false);
           }
-        }, 2000);
+        }, 1500);
       })
       .catch((err) => {
         if (isMounted) {
@@ -37,7 +42,34 @@ const Show = () => {
   if (error) {
     return <div>Error Occured: {error}😖</div>;
   }
-  return <div>showp page</div>;
+  return (
+    <ShowPageWrapper>
+      <ShowMainData
+        id={show.id}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+        image={show.image}
+      />
+      <InfoBlock>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </InfoBlock>
+      <InfoBlock>
+        <h2>Seasons</h2>
+        <Season seasons={show._embedded.seasons} />
+      </InfoBlock>
+      <InfoBlock>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} image={show.image} />
+      </InfoBlock>
+    </ShowPageWrapper>
+  );
 };
 
 export default Show;
